ade08e7e11f57cfa8c3b54186eeaf8622b615366
Benjamin Vincent Shalom Berthelot -X (beberthe - TRITON UK BIDCO LIMITED c/o Alter Domus (UK) Limited -OBO at Cisco) <bberthelot@synamedia.com>
2021-09-19 10:50:03 +0300

    code changes

--- handler.py
+++ handler.py
@@ -1,4 +1,12 @@
+import boto3
 from datetime import date, datetime
+from flask import Flask, jsonify, request, abort
+import json
+
+app = Flask(__name__)
+BASE_ROUTE = "/"
+
+REGION_FILE_NAME = "regions.txt"
 
 
 # Helper to translate AWS datatime to ISO format
@@ -7,3 +15,51 @@ def datetime_converter(obj):
         return obj.isoformat()
     raise TypeError("Type {} is not serializable".format(type(obj)))
 
+
+def get_region_from_file():
+    f = open(REGION_FILE_NAME, 'r')
+    data = f.read()
+    f.close()
+    return data
+
+
+def config_region(region_data):
+    f = open(REGION_FILE_NAME, 'w+')
+    f.write(region_data)
+    f.close()
+
+
+def get_instances(regions):
+    instances_list = []
+    ec2client = boto3.client('ec2', region_name=regions)
+    response = ec2client.describe_instances()
+    for r in response['Reservations']:
+        for i in r['Instances']:
+            instances_list.append(i)
+    return instances_list
+
+
+def etl():
+    regions_name = get_region_from_file()
+    print(regions_name)
+    instances_list = get_instances(regions_name)
+    sorted_instances_list = instances_list.sort(key=lambda x: datetime_converter(x['launch_time']))
+    with open(regions_name + '.json', 'w+') as f:
+        json.dump(sorted_instances_list, f)
+
+
+@app.route(BASE_ROUTE, methods=['GET'])
+def lambda_handler(event, context):
+    try:
+        args = request.args
+        if args and request.args.get('region'):
+            region = request.args.get('region')
+            config_region(region)
+            etl()
+            return jsonify()
+        else:
+            abort(404, "the request should contain the region param")
+
+
+    except:
+        print("An exception occurred during the process")
